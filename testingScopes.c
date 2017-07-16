f265591f1ae320bb92d7a86c85187ad96d26fec0
Hareesh <Hareesh.Puliyedath@gmail.com>
2017-07-16 14:28:52 -0700

    understanding c scopes

---
+++ testingScopes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//this is a test
+int first ;
+
+int main()
+{
+  printf ("first is %d\n", first);
+
+
+  if (1)
+    {
+      first = 0 ;
+      printf("first is %d\n", first);
+    }
+
+  printf("first is %d\n", first);
+  return 0 ;
+}
+
+int first = 121;
+
+
+/*
+Scope rules - header files , static variables - interal statci 
+ */
+
+
+
+
+
