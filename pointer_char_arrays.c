8774bbc8110c3ceeac84e89a160a5bc75e1164ed
Hareesh <Hareesh.Puliyedath@gmail.com>
2017-07-25 13:40:43 -0700

    added str length with 1. array subscript 2. and pointer increments

--- pointer_char_arrays.c
+++ pointer_char_arrays.c
@@ -1,7 +1,33 @@
-/* exercises with pointers and char arrays */
 #include <stdio.h>
+
+#define EOS '\0'
 
 int main()
 {
+  int str_len_pointer(char *s);
+  char s[] = "hari";
+  printf("%d",str_len_pointer(s));
   return 0;
+}
+
+int str_len(char s[])
+{
+  int i = 0 ;
+  while ( s[i] != EOS )
+    {
+      i++;
+    }
+
+  return i ;
+}
+
+int str_len_pointer(char *s)
+{
+  char *p = s ;
+  while(*p != EOS)
+    {
+      p++;
+    }
+
+  return p - s;
 }
