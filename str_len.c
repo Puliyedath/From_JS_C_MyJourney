2e5f9ca04498ce342b1580fe236acd9f1d63a75c
Hareesh <Hareesh.Puliyedath@gmail.com>
2017-07-25 17:15:10 -0700

    pointers and arrays and string constants - confusion is gone

--- str_len.c
+++ str_len.c
@@ -6,18 +6,18 @@ int main()
   void str_cpy(char *s , char *t);
   char s[] = "hareesh";
   char t[] = "Aneesh";
-  //str_cpy(s , t);
-  str_change_ptr(s, t);
-  printf("%s-%s\n", s,t);
+  str_change_ptr("haeeesh", "aneesh");
   return 0;
 }
 
 void str_change_ptr(char *s, char *t)
 {
-  while (*s = *t){
-    *s++;
-    *t++;
-  }
+  *t = *s++;
+  printf("%c\n", *s);
+  /* while (*s = *t){ */
+  /*   *s++; */
+  /*   *t++; */
+  /* } */
 }
 
 void str_cpy(char *s , char *t)
