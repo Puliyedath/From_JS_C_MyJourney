7552ad8b44e621529e3eb49a91ff5212282f89a0
Hareesh <Hareesh.Puliyedath@gmail.com>
2017-12-04 11:48:13 -0800

    undestanding symbolic constants

---
+++ symbolic_constants.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#define hareesh 3000 //this is just a string of characters even though they are numbers
+
+int main()
+{
+  printf("the value of adding plus one to a define variable is %d", (hareesh + 1));
+}
